b29dbcd61b0aae9be234302d92a38055985e8cd1
Asif Nawaz <asifnawaz1220@gmail.com>
2023-12-02 13:10:04 +0500

    Add files via upload

---
+++ precache-manifest.1c856f8a632d5d6aa40904b2f07d01cd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf0f02bed492e08d420b8d41c1eb820",
+    "url": "DB1.png"
+  },
+  {
+    "revision": "58fa529026dc4204978c7d9099c6f75a",
+    "url": "DoubleArmStreetLightPole.png"
+  },
+  {
+    "revision": "8b0c583220498cc60fe6cfcc616ee924",
+    "url": "GENERATOR.png"
+  },
+  {
+    "revision": "4c828fb7e815a02ba9214b3b6d99f1ab",
+    "url": "HouseConnection.png"
+  },
+  {
+    "revision": "19e3762a390cb3a7dadd91b540c6b57f",
+    "url": "OrdinaryEarth.png"
+  },
+  {
+    "revision": "ddbca93246c22ad9372d41d909c11143",
+    "url": "Screenshot_7 copy.png"
+  },
+  {
+    "revision": "300057eb3b10dfb75d869c451ebeebbb",
+    "url": "SepticTank.png"
+  },
+  {
+    "revision": "39ba19b0c0b76895a6b0a50182cf7b50",
+    "url": "SpacialEarth.png"
+  },
+  {
+    "revision": "f168c12d17b3d51c56618eb5065c1e84",
+    "url": "Transformer.png"
+  },
+  {
+    "revision": "a383d3bb4d734e147db9",
+    "url": "css/app.606fdf3d.css"
+  },
+  {
+    "revision": "7e207062daae4adc46ed",
+    "url": "css/chunk-vendors.6b17c127.css"
+  },
+  {
+    "revision": "c299315f2c6bd84a442cefb66c869969",
+    "url": "data/Plots_Approved.json"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f18ec11af723134435af62edac91c9b4",
+    "url": "index.html"
+  },
+  {
+    "revision": "ff2e8f6dd00976a82c1a",
+    "url": "js/about.c610d7ea.js"
+  },
+  {
+    "revision": "a383d3bb4d734e147db9",
+    "url": "js/app.54a3d63a.js"
+  },
+  {
+    "revision": "7e207062daae4adc46ed",
+    "url": "js/chunk-vendors.6406a51f.js"
+  },
+  {
+    "revision": "554fa87c776c032b160248efebff29a5",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5b240320a255aa25672db0c4cbcf2e31",
+    "url": "server.js"
+  },
+  {
+    "revision": "df7767e54f9e94c07d6d96518cfbcb10",
+    "url": "valve-1.png"
+  }
+]);
